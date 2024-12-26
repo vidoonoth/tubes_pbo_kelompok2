8fecf4b7781bafc281680ee42e37c7d08c6667da
vidoonoth <alfinvidoo@gmail.com>
2024-12-26 16:12:32 +0700

    menambahkan routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PengusulController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('pengusul', [PengusulController::class, 'index'])->name('pengusul');  // API untuk mengambil data
+Route::post('pengusul', [PengusulController::class, 'store'])->name('pengusul'); // API untuk menyimpan data
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
